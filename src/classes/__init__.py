50cd891a961e9834a30b8f454d7458cc40878475
João Pedro Dias Caparroz <joao.diascaparroz@gmail.com>
2024-09-23 23:02:57 -0300

    feat: exception class to handle errors

--- src/classes/__init__.py
+++ src/classes/__init__.py
@@ -1,3 +1,5 @@
 from .Timeit import Timeit
 from .TimeitHistorical import TimeitHistorical
+from .TimeitHistorical import create_timeit_historical_from_json
+from .Excepts import InvalidTimeitData
 from .TNotion import TNotion
